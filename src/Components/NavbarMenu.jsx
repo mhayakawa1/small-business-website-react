2a9dff7b6e135773340e1d62927ee1c9bcb568cf
makihaya <93355988+makihaya@users.noreply.github.com>
2023-03-13 20:32:06 -0700

    Create NavbarMenu.jsx

---
+++ src/Components/NavbarMenu.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import { NavLink } from 'react-router-dom';
+{/*      
+*/}          
+          
+function NavbarMenu(){
+    const linkStyles = ({ isActive }) => ({
+      color: '#173935',
+      background: isActive ? '#E8908F' : '#E9A8A5',
+      textDecoration: 'none',
+      paddingTop: '1.25rem',
+      textAlign: 'center',
+      fontFamily: 'Tajawal, sans-serif'
+    })
+        
+    return(
+      <div className='navbar-menu'>
+        <NavLink to='/' className='col-md-1 navbar-link'
+          style={linkStyles}>Home</NavLink>
+        <NavLink to='/shop' className='col-md-1 navbar-link'
+          style={linkStyles}>Shop</NavLink>
+        <NavLink to='/cart' className='col-md-1 navbar-link cart-nav-button'
+          style={linkStyles}>
+          <i className='fas fa-cart-shopping navbar-icon'></i>
+          Cart
+        </NavLink>
+        {/*<NavLink to='/checkout' className='col-md-1 navbar-link cart-nav-button'
+          style={linkStyles}>Checkout</NavLink>*/}
+      </div>
+    )
+}
+export default NavbarMenu;
