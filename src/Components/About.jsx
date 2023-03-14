0cb16d082b6c01c79af6886236d28a230d6fda81
makihaya <93355988+makihaya@users.noreply.github.com>
2023-03-13 20:31:48 -0700

    Create About.jsx

---
+++ src/Components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Section6(){
+    return(
+        <div className='section about-section'>
+            <h2>About</h2>
+            <div className='about-text'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    aliqua. Cras ornare arcu dui vivamus arcu felis bibendum. 
+                    Pretium quam vulputate dignissim suspendisse in est ante 
+                    in nibh. Nisl condimentum id venenatis a condimentum vitae 
+                    sapien. Sociis natoque penatibus et magnis dis. Sed adipiscing 
+                    diam donec adipiscing tristique risus nec feugiat.</p>
+                <p>Nullam vehicula ipsum a arcu cursus vitae congue mauris. 
+                    Porttitor eget dolor morbi non arcu risus quis varius. 
+                    Eget nunc lobortis mattis aliquam. Ridiculus mus mauris 
+                    vitae ultricies leo. Faucibus scelerisque eleifend donec 
+                    pretium vulputate sapien nec sagittis.</p>
+            </div>
+            <div className='about-image'>
+            </div>
+            
+        </div>
+    )
+}
+export default Section6;
