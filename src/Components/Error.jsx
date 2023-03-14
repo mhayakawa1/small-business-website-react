a472b310f5cb48b9a54c292dde622989ef928ba5
makihaya <93355988+makihaya@users.noreply.github.com>
2023-03-14 16:15:32 -0700

    Update Error.jsx

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 function Error(){
     return(
-        <div>
+        <div className='error-page'>
+            <img className='error-background' src='https://images.pexels.com/photos/4207679/pexels-photo-4207679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
             <h3>This page does not exist.</h3>
         </div>
     )
