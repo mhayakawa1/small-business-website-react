0ef3b17d52612eb5efdbd287771ab4958675f888
makihaya <93355988+makihaya@users.noreply.github.com>
2023-03-15 08:57:35 -0700

    Update Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -242,8 +242,11 @@ function Cart(){
             return num
         }
         if(subtotal === 0){
+            console.log('test a')
             for(let i = 0; i < products.length; i++){
+                console.log('test b')
                 if(products[i][2] > 0){
+                    console.log('test c')
                     subtotal = subtotal + (Number(products[i][1]) * products[i][2])
                     console.log(subtotal)
                 }
