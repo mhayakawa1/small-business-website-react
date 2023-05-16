c0da6bf3b2e32cccca83a8a4fd485817f91b1b5b
makihaya <93355988+makihaya@users.noreply.github.com>
2023-05-16 09:44:53 -0700

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -68,29 +68,29 @@ function Home(){
                     <div className='bestseller-card bestseller-1'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/4022206/pexels-photo-4022206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info b-c-info-1'>
-                            <p className='bestseller-card-p'>Bestseller 1</p>
-                            <p className='bestseller-card-p'>$XX.XX</p>
+                            <p className='bestseller-card-p font-extra-small'>Bestseller 1</p>
+                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
                         </div>
                     </div>
                     <div className='bestseller-card bestseller-2'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/4466625/pexels-photo-4466625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
-                        <div className='bestseller-card-info'>
-                            <p className='bestseller-card-p'>Bestseller 2</p>
-                            <p className='bestseller-card-p'>$XX.XX</p>
+                        <div className='bestseller-card-info bestseller-card-info-2'>
+                            <p className='bestseller-card-p font-extra-small'>Bestseller 2</p>
+                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
                         </div>                        
                     </div>
                     <div className='bestseller-card bestseller-3'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/5414339/pexels-photo-5414339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
-                        <div className='bestseller-card-info'>
-                            <p className='bestseller-card-p'>Bestseller 3</p>
-                            <p className='bestseller-card-p'>$XX.XX</p>
+                        <div className='bestseller-card-info bestseller-card-info-2'>
+                            <p className='bestseller-card-p font-extra-small'>Bestseller 3</p>
+                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
                         </div>                        
                     </div>
                     <div className='bestseller-card bestseller-4'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/8903960/pexels-photo-8903960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info'>
-                            <p className='bestseller-card-p'>Bestseller 4</p>
-                            <p className='bestseller-card-p'>$XX.XX</p>
+                            <p className='bestseller-card-p font-extra-small'>Bestseller 4</p>
+                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
                         </div>                        
                     </div>
                 </div>
@@ -137,38 +137,42 @@ function Home(){
                     </div>
                     <div className='review-card'>
                         <img className='review-card-image' src='https://images.pexels.com/photos/15765427/pexels-photo-15765427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
-                        <p className='review-stars'>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                        </p>
-                        <p className='review-text font-extra-small'>
-                            Tempor id eu nisl nunc mi ipsum faucibus vitae 
-                            aliquet. Gravida dictum fusce ut placerat.
-                        </p>
-                        <div className='review-attribution'>
-                            <p className='review-name'>- Dolor Sit Amet</p>
-                            <p className='review-source'><i className='fab fa-yelp review-source'></i> Yelp</p>
-                        </div>
+                        <div className='review-content'>
+                            <p className='review-stars'>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                            </p>
+                            <p className='review-text font-extra-small'>
+                                Tempor id eu nisl nunc mi ipsum faucibus vitae 
+                                aliquet. Gravida dictum fusce ut placerat.
+                            </p>
+                            <div className='review-attribution'>
+                                <p className='review-name'>- Dolor Sit Amet</p>
+                                <p className='review-source'><i className='fab fa-yelp review-source'></i> Yelp</p>
+                            </div>
+                        </div>    
                     </div>
                     <div className='review-card'>
                         <img className='review-card-image' src='https://images.pexels.com/photos/8605039/pexels-photo-8605039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
-                        <p className='review-stars'>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                            <i className='fas fa-star'></i>
-                        </p>
-                        <p className='review-text font-extra-small'>
-                            Maecenas volutpat blandit aliquam etiam. Morbi quis commodo 
-                            odio aenean sed adipiscing diam! Gravida dictum fusce ut placerat.
-                        </p>
-                        <div className='review-attribution'>
-                            <p className='review-name'>- Adipiscing Elit</p>
-                            <p className='review-source'><i className='fab fa-facebook review-source'></i> Facebook</p>
+                        <div className='review-content'>
+                            <p className='review-stars'>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                                <i className='fas fa-star'></i>
+                            </p>
+                            <p className='review-text font-extra-small'>
+                                Maecenas volutpat blandit aliquam etiam. Morbi quis commodo 
+                                odio aenean sed adipiscing diam! Gravida dictum fusce ut placerat.
+                            </p>
+                            <div className='review-attribution'>
+                                <p className='review-name'>- Adipiscing Elit</p>
+                                <p className='review-source'><i className='fab fa-facebook review-source'></i> Facebook</p>
+                            </div>
                         </div>
                     </div>
                 </div>
