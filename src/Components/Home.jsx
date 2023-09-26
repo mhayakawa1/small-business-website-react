05ebbae74b74f0e1a412114f88fb6e869a16a478
makihaya <93355988+makihaya@users.noreply.github.com>
2023-09-25 17:47:23 -0700

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -41,11 +41,8 @@ function Home(){
             <div className='hero-container'>
                 <div className='hero-headline'>
                     <img className='logo' src={logo}></img>
-                    <h1>Lorem<br/>Ipsum Dolor</h1>
-                    <h2>Flower Shop</h2>
-                    <p className='font-extra-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna 
-                    aliqua.</p>
+                    <h1>Green<br/>Oasis Florist</h1>
+                    <p className='font-extra-small'>Order fresh flowers online today! Follow us on social media for floral inspiration.</p>
                     <ul className='social-media-hero'>
                         <li>
                             <a><i className='fab fa-instagram' title='Instagram' alt='instagram logo'></i></a>
@@ -68,37 +65,36 @@ function Home(){
                     <div className='bestseller-card bestseller-1'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/4022206/pexels-photo-4022206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info b-c-info-1'>
-                            <p className='bestseller-card-p font-extra-small'>Bestseller 1</p>
-                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
+                            <p>Lorem Ipsum</p>
+                            <p>$10.00</p>
                         </div>
                     </div>
                     <div className='bestseller-card bestseller-2'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/4466625/pexels-photo-4466625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info bestseller-card-info-2'>
-                            <p className='bestseller-card-p font-extra-small'>Bestseller 2</p>
-                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
+                            <p>Consectetur Adipiscing<br/>$10.00</p>
                         </div>                        
                     </div>
                     <div className='bestseller-card bestseller-3'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/5414339/pexels-photo-5414339.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info bestseller-card-info-2'>
-                            <p className='bestseller-card-p font-extra-small'>Bestseller 3</p>
-                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
+                            <p>Elit Sed Do</p>
+                            <p>$12.00</p>
                         </div>                        
                     </div>
                     <div className='bestseller-card bestseller-4'>
                         <img className='bestseller-card-image' src='https://images.pexels.com/photos/8903960/pexels-photo-8903960.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
                         <div className='bestseller-card-info'>
-                            <p className='bestseller-card-p font-extra-small'>Bestseller 4</p>
-                            <p className='bestseller-card-p font-extra-small'>$XX.XX</p>
-                        </div>                        
+                            <p>Dolor Sit Amet</p>
+                            <p>$12.00</p>
+                        </div>
                     </div>
                 </div>
                 <div className='bestsellers-header'>
                     <h3>Our Bestsellers</h3>
-                    <p className='bestsellers-text font-extra-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Tempor id eu nisl nunc mi ipsum faucibus vitae aliquet.</p>
+                    <p className='bestsellers-text font-extra-small'>Explore our bestselling items and make moments more memorable
+                     with the gift of flowers. Whether it's a birthday, anniversary, or any other milestone, our bouquets add a 
+                     touch of elegance and celebration to your gatherings.</p>
                     <NavLink to='/shop' className='nav-link-button'>
                     <button className='col-xs-1'>
                         See More<i className='fas fa-arrow-right'></i>
@@ -109,11 +105,9 @@ function Home(){
 
             <div className='customer-reviews'>
                 <h3>Customer Reviews</h3>
-                <p className='review-p'>Nullam non nisi est sit amet facilisis. 
-                        Ut venenatis tellus in metus. Sit amet commodo nulla 
-                        facilisi nullam. Lorem ipsum dolor sit amet consectetur 
-                        adipiscing elit ut aliquam. Tempor id eu nisl nunc mi 
-                        ipsum faucibus vitae aliquet. Gravida dictum fusce ut placerat.</p>
+                <p className='review-p'>From weddings and birthdays to everyday surprises, our customers have trusted us to 
+                make their moments truly special. Your feedback inspires us to continue crafting unforgettable floral 
+                arrangements and providing exceptional service.</p>
                 <div className='reviews-container'>
                     <div className='review-card'>
                         <img className='review-card-image' src='https://images.pexels.com/photos/5706258/pexels-photo-5706258.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
@@ -126,11 +120,12 @@ function Home(){
                                 <i className='fas fa-star'></i>
                             </p>
                             <p className='review-text font-extra-small'>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua!
+                            "I received a surprise delivery from Green Oasis today, and I couldn't be happier. 
+                            The bouquet is a masterpiece of colors and fragrances. It brightened my entire day, and 
+                            I feel so appreciated."
                             </p>
                             <div className='review-attribution'>
-                                <p className='review-name'>- Lorem Ipsum</p>
+                                <p className='review-name'>- Daniel White</p>
                                 <p className='review-source'><i className='fab fa-yelp review-source-icon'></i> Yelp</p>
                             </div> 
                         </div>
@@ -146,11 +141,12 @@ function Home(){
                                 <i className='fas fa-star'></i>
                             </p>
                             <p className='review-text font-extra-small'>
-                                Tempor id eu nisl nunc mi ipsum faucibus vitae 
-                                aliquet. Gravida dictum fusce ut placerat.
+                            "I ordered a sympathy bouquet from Green Oasis during a difficult time, and I was deeply 
+                            touched by the beauty and thoughtfulness of the arrangement. It brought comfort and solace 
+                            to our family during a challenging period."
                             </p>
                             <div className='review-attribution'>
-                                <p className='review-name'>- Dolor Sit Amet</p>
+                                <p className='review-name'>- Jessica Garcia</p>
                                 <p className='review-source'><i className='fab fa-yelp review-source'></i> Yelp</p>
                             </div>
                         </div>    
@@ -166,11 +162,12 @@ function Home(){
                                 <i className='fas fa-star'></i>
                             </p>
                             <p className='review-text font-extra-small'>
-                                Maecenas volutpat blandit aliquam etiam. Morbi quis commodo 
-                                odio aenean sed adipiscing diam! Gravida dictum fusce ut placerat.
+                            "I'm a regular customer at Green Oasis, and I can say that they never disappoint.
+                             The staff is incredibly friendly and knowledgeable, always helping me find the perfect 
+                             arrangement for any occasion."
                             </p>
                             <div className='review-attribution'>
-                                <p className='review-name'>- Adipiscing Elit</p>
+                                <p className='review-name'>- Michelle Scott</p>
                                 <p className='review-source'><i className='fab fa-facebook review-source'></i> Facebook</p>
                             </div>
                         </div>
@@ -181,15 +178,11 @@ function Home(){
             <div className='about'>
                 <div className='about-text'>
                     <h3>About</h3>
-                    <p className='font-extra-small'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                        sed do eiusmod tempor incididunt ut labore et dolore magna 
-                        aliqua. Tempor id eu nisl nunc mi ipsum faucibus vitae 
-                        aliquet. <br/><br/>Maecenas volutpat blandit aliquam etiam. Morbi 
-                        quis commodo odio aenean sed adipiscing diam. Gravida 
-                        dictum fusce ut placerat. Nullam non nisi est sit amet 
-                        facilisis. Ut venenatis tellus in metus. Sit amet commodo 
-                        nulla facilisi nullam. Lorem ipsum dolor sit amet 
-                        consectetur adipiscing elit ut aliquam.
+                    <p className='font-extra-small'>
+                    In our early days, Green Oasis operated out of a modest greenhouse, where we handpicked each bloom and crafted 
+                    unique arrangements for local customers.<br/><br/>As our business grew, we expanded our offerings to include floral 
+                    design services for weddings, events, and other occasions. Our mission remains unwavering: to bring the magic 
+                    of flowers to the desert, brightening the lives of those who call Phoenix home and those who visit the city.
                     </p>
                 </div>
                 <div className='about-image'>
