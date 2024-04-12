b6dcef9b267ca0337968c40bcf196e06a241ec25
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-04-11 22:04:30 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -138,7 +138,7 @@ function App() {
               <a className='footer-link'><i className='fab fa-tiktok' title='TikTok' alt='tiktok logo'></i></a>
             </li>
           </ul>
-          <p className='font-extra-small shop-hours'>Hours: 9:30am-7:00pm<br/>Sunday-Saturday</p>
+          <p className='font-extra-small shop-hours'>Hours: 9:30am-7:00pm<br/>Monday-Saturday</p>
         </div>
         <div className='footer-col footer-3'>
           <ul className='footer-list other-information font-extra-small'>
