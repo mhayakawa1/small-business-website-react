644131cecc69556e4922233baf4579122a696b99
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2024-06-02 22:06:07 -0700

    Update Error.jsx

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import HomeBackground from '../OtherImages/Home Background Image.jpeg';
 
 function Error(){
     return(
         <div className='error-page'>
-            <img className='error-background' src='https://images.pexels.com/photos/4207679/pexels-photo-4207679.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'></img>
+            <img className='error-background' src={HomeBackground}></img>
             <h3>This page does not exist.</h3>
         </div>
     )
