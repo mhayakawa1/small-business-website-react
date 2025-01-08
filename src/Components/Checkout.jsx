be4ec3d5a1f77a8c67dda141ea4d9ffb5efff9fd
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-08 08:48:52 -0700

    Update Checkout.jsx

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
-import axios from 'axios';
+import { useCart } from '../Contexts/CartItemsContext';
 
 function Checkout({cart, clickHandler}){
-    const [isLoading, setIsLoading] = useState(false);
+    const { productsData } = useCart();
     const [inCart, setInCart] = useState('');
     const [subtotal, setSubtotal] = useState(0);
     let percentOff = 0;
@@ -33,35 +33,6 @@ function Checkout({cart, clickHandler}){
     let submittedCode = ''
     const [codeValid, setCodeValid] = useState('');
     const [radioBtnVal, setRadioBtnVal] = useState('')
-
-    const [productsData, setProductsData] = useState([]);
-    
-    function parseCSV(csvText) {
-        const rows = csvText.split(/\r?\n/);
-        const headers = rows[0].split(',');
-        const data = [];
-        for (let i = 1; i < rows.length; i++) {
-            const rowData = rows[i].split(',');
-            const rowObject = {};
-            for (let j = 0; j < headers.length; j++) {
-                rowObject[headers[j]] = rowData[j];
-            }
-            data.push(rowObject);
-        }
-        return data;
-    }
-
-    const fetchProductsData = () => {
-        const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ4r5F3JQ2tlhqi0PnFhlBvHcY-W-DWceYwlKITFz9afma_JAwDmH56Kmywig9tWNsxkUZ64MGT3Nnp/pub?output=csv';
-        axios.get(url)
-        .then((response) => {
-            const parsedData = parseCSV(response.data);
-            setProductsData(parsedData);
-        })
-        .catch((error) => {
-            console.error('Error fetching CSV data:', error);
-        })
-    }
 
     const onStorageUpdate = (e) => {
         const { key, newValue } = e;
@@ -93,7 +64,6 @@ function Checkout({cart, clickHandler}){
     }
 
     useEffect(() => {
-        fetchProductsData();
         setInCart(localStorage.getItem('inCart') || '');
         setSubtotal(localStorage.getItem('subtotal') || '');
         window.addEventListener('storage', onStorageUpdate);
