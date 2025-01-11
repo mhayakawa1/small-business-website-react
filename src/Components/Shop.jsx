a15cda79833703fbade75f69ca1a6585c02470e3
M. Hayakawa <93355988+mhayakawa1@users.noreply.github.com>
2025-01-11 09:29:26 -0700

    Update Shop.jsx

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Category from './Category';
 
-function Shop({clickHandler, cart, products}){
+function Shop({clickHandler, cart}){
     const { category } = useParams();
   
     return (
       <>
-        <Category category={category} clickHandler={clickHandler} cart={cart} products={products}/>
+        <Category category={category} clickHandler={clickHandler} cart={cart}/>
       </>
     );
 }
